7bb043de7e02bd408fad93fa5e3c34510c6abb43
Nicholas Fausti <nick@Nicholass-MacBook-Pro.local>
2017-07-11 22:13:35 -0400

    added some css changes

--- src/App.js
+++ src/App.js
@@ -3,11 +3,37 @@ import Videos from './Components/Videos';
 import './App.css';
 import $ from 'jquery';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import NavBar from './Components/NavBar';
 
 class App extends React.Component {
+  constructor() {
+    super();
 
+    this.state = {
+      videos: [],
+      position: [0,0]
+    }
+  }
 
-componentDidMount() {
+  componentDidMount() {
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(savePosition);
+        } else {
+            alert("Geolocation is not supported by this browser.");
+        }
+    }
+
+   function savePosition(position) {
+        var lat = position.coords.latitude;
+        var lng = position.coords.longitude;
+        localStorage['lat'] = lat;
+        localStorage['lng'] = lng;
+        this.setState({position:[lat, lng]})
+
+  //        drawMap(lat, lng);
+    }
+
     this.setState({videos: [
       {
         id: 1,
@@ -33,84 +59,50 @@ componentDidMount() {
         coverImage: "http://link/to/image3",
         sortOrder: 2
       },
-    ]});
+    ]}
+  );
 
-    // function drawMap(lat, lng) {
-    //     var mymap = L.map('mapid').setView([lat, lng], 13);
-    //     L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}',{
-    //     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    //     maxZoom: 18,
-    //     accessToken: 'pk.eyJ1IjoibmNmYXVzdGkiLCJhIjoiY2ozb2M2bnU0MDA1NTJxbjQzOGZob2thYyJ9.Xkb98mlUAX218AuCyfUdiA'
-    //   }).addTo(mymap);
-    // }
+  if(localStorage['lat']){
+      console.log(localStorage);
+      this.setState({
+        position: [ localStorage['lat'], localStorage['lng']]
+      });
+    }
+    else {
+      getLocation();
+    }
 
-    // function getLocation() {
-    //     if (navigator.geolocation) {
-    //         navigator.geolocation.getCurrentPosition(savePosition);
-    //     } else {
-    //         alert("Geolocation is not supported by this browser.");
-    //     }
-    // }
+  var token_str = window.location.href.split('=')[1];
+  if(token_str != undefined)
+    var spotify_token = token_str.substr(0, token_str.length - 11);
 
-    // function savePosition(position) {
-    //     var lat = position.coords.latitude;
-    //     var lng = position.coords.longitude;
-    //     localStorage['lat'] = lat;
-    //     localStorage['lng'] = lng;
-
-    //     drawMap(lat, lng);
-    // }
-
-    // if(localStorage['lat']){
-    //     console.log(localStorage);
-    //     var lat = localStorage['lat'];
-    //     var lng = localStorage['lng'];
-    //     drawMap(lat, lng);
-    //   }
-    //   else {
-    //     getLocation();
-    //   }
-
-      // $('.button-collapse').sideNav();
-        // var mymap = L.map('mapid').setView([39.9526, -75.1652], 13);
-      // var token_str = window.location.href.split('=')[1];
-      // var spotify_token = token_str.substr(0, token_str.length - 11);
-
-      // if(spotify_token)
-      //   localStorage['spotify_token'] = spotify_token;
-
+  if(spotify_token)
+    localStorage['spotify_token'] = spotify_token;
       // Place marker
       // Make ajax call to get current track
       // Show user profile image
-
   }
-
-
-  constructor() {
-    super();
-    this.state = {
-      videos: []
-    }
-
-
+  componentWillUpdate() {
+    this.setState()
   }
 
   render() {
-    const position = [51.505, -0.09]
+    let position = [localStorage.lat, localStorage.lng]//[51.505, -0.09]
 
     return (
       <div className="App">
-       <Map center={position} zoom={13}>
-    <TileLayer
-      url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-      attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    />
-    <Marker position={position}>
-      <Popup>
-        <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
-      </Popup>
-    </Marker>
-  </Map>
+       <NavBar />
+       <Map center={this.state.position} zoom={13}>
+        <TileLayer
+          url='https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibmNmYXVzdGkiLCJhIjoiY2ozb2M2bnU0MDA1NTJxbjQzOGZob2thYyJ9.Xkb98mlUAX218AuCyfUdiA'
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <Marker position={position}>
+          <Popup>
+            <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
+          </Popup>
+        </Marker>
+       </Map>
        <Videos videos={this.state.videos} />
       </div>
     );
---
+++ src/Components/NavBar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+// Video url placeholder
+class NavBar extends React.Component {
+  render() {
+    return (
+      <div className="nav">
+      </div>
+    );
+  }
+}
+//
+// var item = 9;
+//
+// var TodoList = React.createClass({
+//   render: function() {
+//     function item(itemText) {
+//       return <li>{itemText}</li>;
+//     };
+//     return <ul>{this.props.items.map(item)}</ul>;
+//   }
+// });
+
+export default NavBar;
